fe2fb5c2ecb2e23a960eb554ce7e3662d0edd50b
Juan Lima <juan_jpl@hotmail.com>
2023-08-14 01:56:20 -0300

    update v2.0.4

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 //pedido de datos
-import datos from './data.js';
-//console.log(datos);
+import datos from './datos.js';
 
 const verificarIngreso = (texto) => {
     let ingreso;
@@ -12,28 +11,11 @@ const verificarIngreso = (texto) => {
     return ingreso;
   };
 
-  let acc_ingreso=0;
-  let acc_egreso=0;
 
-for (let i=0; i < datos.length-1; i++){
-   
-
-    const ingreso = verificarIngreso(`Ingrese el valor de INGRESO del mes ${datos[i]['mes']}:`);
-    datos[i]['ingreso']=ingreso;
-    acc_ingreso += ingreso;
-  
-    const egreso = verificarIngreso(`Ingrese el valor de EGRESO del mes ${datos[i]['mes']}:`);
-    datos[i]['egreso']=egreso;
-    acc_egreso += egreso;
-    //console.log(`${datos[i]['mes']}\t\t` , `Ingreso: ${datos[i]['ingreso']}\t`,`Egreso: ${datos[i]['egreso']}`)
+for (let i=0; i < datos.length; i++){
+    datos[i]['interes']= ((datos[i]['capital'])*(datos[i]['plazo'])*(datos[i]['tasa']))/100;
 }
 
-datos[12]['ingreso']=acc_ingreso;
-datos[12]['egreso']=acc_egreso;
 
 
 console.table(datos);
-
-(( acc_ingreso-acc_egreso) < 0 )
-? console.log('CUIDADO! Este flujo de ingresos genera perdidas!')
-: console.log('FELICITACIONES! Este flujo de ingresos genera ganancias!')
