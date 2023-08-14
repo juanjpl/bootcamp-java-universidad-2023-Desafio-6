2723f908665b7df8d6c79a2077f9933e1f1b7c6c
Juan Lima <juan_jpl@hotmail.com>
2023-08-14 01:10:28 -0300

    update v2.0.2

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//pedido de datos
+import datos from './data.js';
+//console.log(datos);
+
+const verificarIngreso = (texto) => {
+    let ingreso;
+  
+    do {
+      ingreso = Number(prompt(`${texto}`));
+    } while (!ingreso || ingreso < 0);
+  
+    return ingreso;
+  };
+
+  let acc_ingreso=0;
+  let acc_egreso=0;
+
+for (let i=0; i < datos.length-1; i++){
+   
+
+    const ingreso = verificarIngreso(`Ingrese el valor de INGRESO del mes ${datos[i]['mes']}:`);
+    datos[i]['ingreso']=ingreso;
+    acc_ingreso += ingreso;
+  
+    const egreso = verificarIngreso(`Ingrese el valor de EGRESO del mes ${datos[i]['mes']}:`);
+    datos[i]['egreso']=egreso;
+    acc_egreso += egreso;
+    //console.log(`${datos[i]['mes']}\t\t` , `Ingreso: ${datos[i]['ingreso']}\t`,`Egreso: ${datos[i]['egreso']}`)
+}
+
+datos[12]['ingreso']=acc_ingreso;
+datos[12]['egreso']=acc_egreso;
+
+
+console.table(datos)
+
+
+
+
+
---
+++ js/data.js
@@ -0,0 +1,71 @@
+
+const datos = [
+    {
+      mes: "Enero",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Febrero",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Marzo",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Abril",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Mayo",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Junio",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Julio",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Agosto",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Septiembre",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Octubre",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Noviembre",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+      mes: "Diciembre",
+      ingreso: 0,
+      egreso: 0,
+    },
+    {
+       mes: "Total",
+        ingreso: 0,
+        egreso: 0,
+      },
+  ];
+  
+
+  export default datos;
