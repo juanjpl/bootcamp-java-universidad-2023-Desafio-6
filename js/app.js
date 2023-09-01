5e3d8ed1febab877e5d6597bb56e2ce833dc2d38
Juan Lima <juan_jpl@hotmail.com>
2023-09-01 00:47:48 -0300

    update v3.0.0

--- js/app.js
+++ js/app.js
@@ -1,29 +1,94 @@
 //pedido de datos
-import datos from './datos.js';
-
-
+import datos from "./datos.js";
 
 const verificarIngreso = (texto) => {
-    let ingreso;
-  
-    do {
-      ingreso = Number(prompt(`${texto}`));
-    } while (!ingreso || ingreso < 0);
-  
-    return ingreso;
-  };
+  let ingreso;
 
+  do {
+    ingreso = Number(prompt(`${texto}`));
+  } while (!ingreso || ingreso < 0);
 
-for (let i=0; i < datos.length; i++){
-    datos[i]['interes']= ((datos[i]['capital'])*(datos[i]['plazo'])*(datos[i]['tasa']))/100;
+  return ingreso;
+};
+
+for (let i = 0; i < datos.length; i++) {
+  datos[i]["interes"] =
+    (datos[i]["capital"] * datos[i]["plazo"] * datos[i]["tasa"]) / 100;
 }
 
+const verificarDatos = (datos) => {
+ 
+
+  console.log('Carga de los datos presentados ')
+  console.log('=====================================')
+ 
+  let array = datos;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].hasOwnProperty("nombre") && typeof array[i]["nombre"] === "string") {
+      //console.log(typeof array[i]["ingreso"]);
+      console.log(array[i]["nombre"]);
+
+      if (
+        array[i].hasOwnProperty("capital") &&
+        typeof array[i]["capital"] === "number"
+      ) {
+        //console.log(typeof array[i]["ingreso"]);
+        console.log(array[i]["capital"]);
+        //acc_ingreso = acc_ingreso + array[i]["plazo"];
+        if (
+          array[i].hasOwnProperty("plazo") &&
+          typeof array[i]["plazo"] === "number"
+        ) {
+          //console.log(typeof array[i]["ingreso"]);
+          
+          console.log(array[i]["plazo"]);
+
+          //acc_egreso = acc_egreso + array[i]["egreso"];
+          if (
+            array[i].hasOwnProperty("tasa") &&
+            typeof array[i]["tasa"] === "number"
+          ) {
+            //console.log(typeof array[i]["ingreso"]);
+            
+            console.log(array[i]["tasa"]);
+  
+            //acc_egreso = acc_egreso + array[i]["egreso"];
+           
+          } else {
+            console.log("error en la carga de datos de Tasa");
+          }
+         
+        } else {
+          console.log("error en la carga de datos de Plazo");
+        }
+      } else {
+        console.log("error en la carga de datos en Capital");
+      }
+    } else {
+      console.log("error en la carga de datos en Nombre");
+    }
+  }
+
+
+  console.log('=====================================')
+  console.log('El resultado de la funcion es.... ')
+//console.log(array)
+
+return array;
+};
 
 
 
-const calcularInteres=(datos)=>{
+const calcularInteres = (datos) => {
+  let respuesta = Array.isArray(datos);
+  let arrayDevuelvo = [];
+  !respuesta
+    ? console.log("Haz ingresado un formato distinto a ARRAY")
+    : (arrayDevuelvo = verificarDatos(datos));
 
-}
+  return arrayDevuelvo;
+};
 
-
-console.table(datos);
+const resultado = calcularInteres(datos);
+console.log(resultado);
--- js/datos.js
+++ js/datos.js
@@ -6,22 +6,49 @@ const datos =[
     capital:150000,
     plazo:30,
     tasa:0.15,
-    interes:0
+  
 },
 {
     nombre:'Plan 002',
     capital:300000,
     plazo:180,
     tasa:0.10,
-    interes:0
+
 },
 {
     nombre:'Plan 003',
     capital:485000,
     plazo:60,
     tasa:0.23,
-    interes:0
+
 },
 ]
+
+
+
+const datos1 = [
+    {
+      mes: 'Enero',
+      ingreso: 10000,
+      egreso: 1000,
+    },
+    {
+      mes: "Febrero",
+      ingreso: 15000,
+      egreso: 1500,
+    },
+    {
+      mes: "Marzo",
+      ingreso: 40000,
+      egreso: 4000,
+    },
+  ];
+  
+  const datos2 = 1;
+  
+  
+
+  
+
 
 export default datos;
