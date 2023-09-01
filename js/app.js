91ca412722f18c8ed6600bea0afdc1439f33e9d1
Juan Lima <juan_jpl@hotmail.com>
2023-08-31 23:54:30 -0300

    update v7.0.0

--- js/app.js
+++ js/app.js
@@ -1,141 +1,93 @@
-//pedido de datos
-/*
-import datos from './data.js';
-//console.log(datos);
-
-const verificarIngreso = (texto) => {
-    let ingreso;
-  
-    do {
-      ingreso = Number(prompt(`${texto}`));
-    } while (!ingreso || ingreso < 0);
-  
-    return ingreso;
-  };
-
-  let acc_ingreso=0;
-  let acc_egreso=0;
-
-for (let i=0; i < datos.length-1; i++){
-   
-
-    const ingreso = verificarIngreso(`Ingrese el valor de INGRESO del mes ${datos[i]['mes']}:`);
-    datos[i]['ingreso']=ingreso;
-    acc_ingreso += ingreso;
-  
-    const egreso = verificarIngreso(`Ingrese el valor de EGRESO del mes ${datos[i]['mes']}:`);
-    datos[i]['egreso']=egreso;
-    acc_egreso += egreso;
-    //console.log(`${datos[i]['mes']}\t\t` , `Ingreso: ${datos[i]['ingreso']}\t`,`Egreso: ${datos[i]['egreso']}`)
-}
-
-datos[12]['ingreso']=acc_ingreso;
-datos[12]['egreso']=acc_egreso;
-
-
-console.table(datos);
-
-(( acc_ingreso-acc_egreso) < 0 )
-? console.log('CUIDADO! Este flujo de ingresos genera perdidas!')
-: console.log('FELICITACIONES! Este flujo de ingresos genera ganancias!')
-
-*/
 
 //Datos para ser probados en la función
 
-const datos1=[
+const datos1 = [
   {
-    mes:'Enero' ,
+    mes: 'Enero',
     ingreso: 10000,
-    egreso:1000,
-
+    egreso: 1000,
   },
   {
-    mes:'Febrero' ,
+    mes: "Febrero",
     ingreso: 15000,
-    egreso:1500,
-
+    egreso: 1500,
   },
   {
-    mes:'Marzo' ,
+    mes: "Marzo",
     ingreso: 40000,
-    egreso:4000,
+    egreso: 4000,
+  },
+];
 
-  }
-]
-
-const datos2= 1;
-
-const verificarArray=(array)=>{
-
-  let respuesta = Array.isArray(array);
-
-  (!respuesta)
-  ?console.log('Haz ingresado un formato distinto a ARRAY')
-  :verificarDatos(array)
-
-}
-
-const verificarDatos =(array)=>{
-
-  let acc_egreso=0;
-  let acc_ingreso=0;
-
-  for(let i=0; i<array.length;i++){
-    if(array[i].hasOwnProperty("mes") && typeof array[i]['mes'] === 'string'){
-      
-      console.log( `Existe la propiedad MES en Array. ${array[i]['mes']} `)
-
-      if(array[i].hasOwnProperty("ingreso") && Number(array[i]['ingreso'])){
-        console.log( `Existe la propiedad INGRESO en Array. ${array[i]['mes']} `)
-
-        if(array[i].hasOwnProperty("egreso") && Number(array[i]['egreso'])){
-          console.log( `Existe la propiedad EGRESO en Array. ${array[i]['mes']} `)
-
-          acc_egreso = acc_egreso+array[i]['egreso'];
-          acc_ingreso =acc_ingreso+array[i]['ingreso'];
-        }else{
-          console.log('error en la carga de datos de.')
-        }
-      }else{
-        console.log('error en la carga de datos de.')
-      }
-    }else{
-      console.log('error en la carga de datos de.')
-    }
+const datos2 = 1;
 
 
+const verificarDatos = (array) => {
+  let acc_egreso = 0;
+  let acc_ingreso = 0;
 
-    //console.log(array[i]['ingreso'])
-  }
-
-  console.log(acc_egreso)
-  console.log(acc_ingreso)
-  console.log(acc_ingreso - acc_egreso)
-  if((acc_ingreso - acc_egreso)>0){
-
-   
-    console.log(1)
-  }else if((acc_ingreso - acc_egreso)===0){
-    
-      console.log(0)
-   
-  }else{
-    console.log(-1)
-  }
-  
-
-
-}
-
-
-const ingresarArray =(array)=>{
-
-  const isArray = verificarArray(array);
-  
+  console.log('Carga de los datos presentados ')
+  console.log('=====================================')
  
 
-}
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].hasOwnProperty("mes") && typeof array[i]["mes"] === "string") {
+      //console.log(typeof array[i]["ingreso"]);
+      console.log(array[i]["mes"]);
+
+      if (
+        array[i].hasOwnProperty("ingreso") &&
+        typeof array[i]["ingreso"] === "number"
+      ) {
+        //console.log(typeof array[i]["ingreso"]);
+        console.log(array[i]["ingreso"]);
+        acc_ingreso = acc_ingreso + array[i]["ingreso"];
+        if (
+          array[i].hasOwnProperty("egreso") &&
+          typeof array[i]["egreso"] === "number"
+        ) {
+          //console.log(typeof array[i]["ingreso"]);
+          
+          console.log(array[i]["egreso"]);
+
+          acc_egreso = acc_egreso + array[i]["egreso"];
+         
+        } else {
+          console.log("error en la carga de datos de Egreso");
+        }
+      } else {
+        console.log("error en la carga de datos en Ingreso");
+      }
+    } else {
+      console.log("error en la carga de datos en Mes");
+    }
+  }
+  console.log('=====================================')
+  console.log(`El total de Ingresos es: ${acc_ingreso}`);
+  console.log(`El total de Egresos es: ${acc_egreso}`);
+  console.log(`Ingresos - Egreso: ${acc_ingreso - acc_egreso}`);
+
+  console.log('=====================================')
+  console.log('El resultado de la funcion es.... ')
+  if (acc_ingreso - acc_egreso > 0) {
+    console.log(1);
+  } else if (acc_ingreso - acc_egreso === 0) {
+    console.log(0);
+  } else {
+    console.log(-1);
+  }
+};
+
+
+const ingresarArray = (array) => {
+  let respuesta = Array.isArray(array);
+
+  !respuesta
+    ? console.log("Haz ingresado un formato distinto a ARRAY")
+    : verificarDatos(array);
+};
+
+
 
 
 ingresarArray(datos1);
